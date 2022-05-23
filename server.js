42e10e964b2f5d50020415c0f64345d78b4a9cb2
Katya Juliet Lerner <katyajuliet22@gmail.com>
2022-05-22 23:16:40 -0700

    heroku port

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const { animals } = require('./data/animals');
 //require express.js
 const express = require('express');
 
+const PORT = process.env.PORT || 3001;
 //instantiate the server
 const app = express();
 
@@ -57,6 +58,6 @@ app.get('/api/animals', (req, res) => {
   });
 
 //make the server listen
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
